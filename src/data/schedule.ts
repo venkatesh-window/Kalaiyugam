4dc5747021d5e4067f5886640a1883ad52fdeeaa
sanjith0x <sanjithcodez@gmail.com>
2026-08-13 23:42:48 +0530

    fix: added sample posters for the 3 events

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -21,10 +21,10 @@ export const WorkshopDay2: EventSlot[] = [
 
 
 export const MainEventDay: EventSlot[] = [
-  { time: "10:00 AM - 12:00 PM", event: "Emotion Premier League (EPL)", venue: "Parthasarathy Auditorium", movement: "Value", registerLink: "https://forms.gle/TAKCeniUyERDxYRh6", price: "60" },
-  { time: "10:00 AM - 12:00 PM", event: "Degrees of Emotion", venue: "Classroom", movement: "Intensity", registerLink: "https://forms.gle/6AH8BUR6yKbduH78A", price: "60" },
+  { time: "10:00 AM - 12:00 PM", event: "Emotion Premier League (EPL)", venue: "Parthasarathy Auditorium", movement: "Value", registerLink: "https://forms.gle/TAKCeniUyERDxYRh6", price: "60", image: "/emotion premier league poster.jpg" },
+  { time: "10:00 AM - 12:00 PM", event: "Degrees of Emotion", venue: "Classroom", movement: "Intensity", registerLink: "https://forms.gle/6AH8BUR6yKbduH78A", price: "60", image: "/degrees of emotion poster.jpg" },
   { time: "10:00 AM - 12:00 PM", event: "Speed Networking", venue: "Classroom", movement: "Belonging", registerLink: "https://forms.gle/sHoKHoJf9nsr6oL27", price: "60", image: "/speed networking poster (1).png" },
   { time: "10:00 AM - 12:00 PM", event: "Hear Me Out", venue: "Lab 1", movement: "Vulnerability", registerLink: "https://forms.gle/s6gP6Ph3VkGwHFQ18", price: "60", image: "/hear me out slide 1 (1).png" },
-  { time: "10:00 AM - 12:00 PM", event: "Emotional Damage", venue: "Lab 2", movement: "Catharsis", registerLink: "https://forms.gle/eetE8zCPHBdj5LzU9", price: "60" },
+  { time: "10:00 AM - 12:00 PM", event: "Emotional Damage", venue: "Lab 2", movement: "Catharsis", registerLink: "https://forms.gle/eetE8zCPHBdj5LzU9", price: "60", image: "/emotional damage poster.jpg" },
   { time: "10:00 AM - 12:00 PM", event: "Illusion to Intuition", venue: "Lab 3", movement: "Wonder", registerLink: "https://forms.gle/6KJb8CH95Eiaf7pY7", price: "60", image: "/Illusion to intuition poster (2).png" },
 ];
